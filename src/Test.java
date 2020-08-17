9dbd02eb92976159cb102ed9be28e10bffaaf4ca
Park-Jun-Kyu <dailykq@naver.com>
2020-08-17 22:58:20 +0900

    switch

--- src/Test.java
+++ src/Test.java
@@ -1,23 +1,38 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        String season = "";
+        // switch문을 이용해서 season이 오늘은 어떤 계절인지 나타내게 만들어보세요.
+        switch(month) {
 
-        String a = sc.next();
-
-        String[]arr= a.split(":");
-
-        for(int i=0; i<arr.length; i++){
-            String b = arr[i];
-            if(i==1){
-                System.out.println(b);
-            }
+            case 1:
+            case 2:
+            case 12:
+                season = "겨울";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                season = "봄";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                season = "여름";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                season = "가을";
+                break;
         }
-
-
+        System.out.println("지금은 " + month + "월이고, " + season + "입니다.");
 
     }
+
 }
